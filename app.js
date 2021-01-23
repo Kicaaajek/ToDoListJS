d0ccde60a7fe9285793d83e80d0d030090ca41c2
Monika Zając <monikaannazajac@gmail.com>
2021-01-24 00:12:38 +0100

    Try to add Databases.

--- app.js
+++ app.js
@@ -1,23 +1,64 @@
 const express=require("express");
-const bodyParser=require("body-parser");
+const bodyParser = require("body-parser");
+const sql = require("mysql");
 const app=express();
 const port=3000;
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
+const db = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'ToDoList'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected");
+});
+
+app.get('/', (req, res) => {
+    let sql = 'CREATE DATABASE ToDoList';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Database created");
+    });
+    let sql = 'CREATE TABLE tasks(id int AUTO_INCREMENT, ToDo VARCHAR(255), PRIMARY KEY(id))';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Table created");
+    });
   res.render('index', {actualDate: actualDate});
 });
 
 app.get('/list',(req,res)=>{
-
-  res.render('list', {actualDate: actualDate, actualDay:actualDay, items:items});
+    var items = new Array();
+    let sql = 'INSERT INTO persons SET ?';
+    let query = db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        items.push(result);
+        res.send("Uploaded");
+    });
+    res.render('list', {actualDate: actualDate, actualDay:actualDay, items:items});
 });
 
 app.post('/list',(req,res)=>{
-  var toDo=req.body.item;
-  items.push(toDo);
+    let post = { ToDo: req.body.item};
+    let sql = 'INSERT INTO tasks SET ?';
+    let query = db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    res.send("Uploaded");
+    });
+  //items.push(toDo);
+  
   res.redirect('/list');
 });
 
@@ -36,16 +77,10 @@ const options={ year: "numeric",
 const opt={weekday: "long"};
 var actualDate=date.toLocaleDateString(locales="pl-UK", options);
 var actualDay = date.toLocaleDateString(locales="pl-UK", opt);
-var items = new Array();
-var workItems = new Array();
-items.push("Umyj naczynia");
-items.push("Zrób zakupy");
-items.push("Zadzwoń do okulisty");
-function addToItemsList(){
-  var a=item.value;
-  items.push(a);
-}
-function removeItem(){
-  //var x=item.value;
-  items.splice(i);
-}
+//var items = new Array();
+//var workItems = new Array();
+//items.push("Umyj naczynia");
+//items.push("Zrób zakupy");
+//items.push("Zadzwoń do okulisty");
+
+
